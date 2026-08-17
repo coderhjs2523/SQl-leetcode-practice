84dac11960c3281ff4f8eb71ccc9f0bc192f3b96
Hareesh <hareeshjaiveersingh@gmail.com>
2026-08-17 08:15:39 +0530

    Time: 388 ms (41.53%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,5 +1,5 @@
 SELECT E1.name
 FROM Employee E1
-JOIN Employee E2 ON E1.id = E2.managerId
-GROUP BY E1.id
-HAVING COUNT(E1.id) >= 5;
+LEFT JOIN Employee E2 ON E2.managerId = E1.id
+GROUP BY E2.managerId
+HAVING count(E2.managerId)>=5;
