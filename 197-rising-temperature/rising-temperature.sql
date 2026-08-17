ea935fa2cd25902831b8f2f80e8dd5e327e7db30
Hareesh <hareeshjaiveersingh@gmail.com>
2026-08-17 08:06:46 +0530

    Time: 507 ms (58.19%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,3 +1,3 @@
-SELECT W1.id
-FROM Weather W1, Weather W2
-WHERE DATEDIFF(W1.recordDate, W2.recordDate) = 1 AND (W1.temperature > W2.temperature);
+SELECT W2.id
+FROM Weather W1, Weather W2 
+WHERE DATEDIFF(W2.recordDate, W1.recordDate)=1 AND W2.temperature > W1.temperature;
