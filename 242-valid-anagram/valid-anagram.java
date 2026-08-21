ebaece94f91698bdc784ff8d7c28037a0a7e45eb
Hareesh <hareeshjaiveersingh@gmail.com>
2026-08-21 08:20:17 +0530

    Time: 21 ms (7.34%) | Memory: 47.5 MB (9.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,42 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        if(s.length() != t.length())
+        if (s.length() != t.length())
             return false;
 
-        HashMap<Character,Integer> mapS = new HashMap<>();
-        HashMap<Character,Integer> mapT = new HashMap<>();
+        HashMap<Character, Integer> mapS = new HashMap<>();
+        HashMap<Character, Integer> mapT = new HashMap<>();
 
-        for(int i=0; i<s.length(); i++){
-            if(mapS.containsKey(s.charAt(i))){
+        for (int i = 0; i < s.length(); i++) {
+            if (mapS.containsKey(s.charAt(i))) {
                 int freq = mapS.get(s.charAt(i));
-                mapS.put(s.charAt(i), freq+1);
-            }
-            else{
+                mapS.put(s.charAt(i), freq + 1);
+            } else {
                 mapS.put(s.charAt(i), 1);
             }
         }
 
-        for(int i=0; i<t.length(); i++){
-            if(mapT.containsKey(t.charAt(i))){
+        for (int i = 0; i < t.length(); i++) {
+            if (mapT.containsKey(t.charAt(i))) {
                 int freq = mapT.get(t.charAt(i));
-                mapT.put(t.charAt(i), freq+1);
-            }
-            else{
+                mapT.put(t.charAt(i), freq + 1);
+            } else {
                 mapT.put(t.charAt(i), 1);
             }
         }
 
-         for (char ch : mapS.keySet()) {
+        for (char ch : mapS.keySet()) {
 
-            if (!mapT.containsKey(ch)) {
+            if (!mapT.containsKey(ch) || !mapS.get(ch).equals(mapT.get(ch))) {
                 return false;
             }
 
-            if (!mapS.get(ch).equals(mapT.get(ch))) {
-                return false;
-            }
         }
-        return true;     
+        return true;
     }
 }
