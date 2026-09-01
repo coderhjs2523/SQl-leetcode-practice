976dab50821f0545ea7cbd840beed48bd586372a
Hareesh <hareeshjaiveersingh@gmail.com>
2026-09-01 19:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (87.24%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,32 +1,54 @@
 class Solution(object):
     def pivotIndex(self, nums):
-        
-        prefix_start = []
-        sum_start = 0
-        
-        for i in range(len(nums)):
-            sum_start += nums[i]
-            prefix_start.append(sum_start)
 
-        prefix_end = [0] * len(nums)
-        sum_end = 0
-        
-        for i in range(len(nums) - 1, -1, -1):
-            sum_end += nums[i]
-            prefix_end[i] = sum_end
+        total = sum(nums)
+        left_sum = 0
 
         for i in range(len(nums)):
-            
-            left_sum = 0
-            right_sum = 0
-            
-            if i > 0:
-                left_sum = prefix_start[i - 1]
-            
-            if i < len(nums) - 1:
-                right_sum = prefix_end[i + 1]
-            
+
+            right_sum = total - left_sum - nums[i]
+
             if left_sum == right_sum:
                 return i
-        
+
+            left_sum += nums[i]
+
         return -1
+
+
+
+
+
+
+# class Solution(object):
+#     def pivotIndex(self, nums):
+        
+#         prefix_start = []
+#         sum_start = 0
+        
+#         for i in range(len(nums)):
+#             sum_start += nums[i]
+#             prefix_start.append(sum_start)
+
+#         prefix_end = [0] * len(nums)
+#         sum_end = 0
+        
+#         for i in range(len(nums) - 1, -1, -1):
+#             sum_end += nums[i]
+#             prefix_end[i] = sum_end
+
+#         for i in range(len(nums)):
+            
+#             left_sum = 0
+#             right_sum = 0
+            
+#             if i > 0:
+#                 left_sum = prefix_start[i - 1]
+            
+#             if i < len(nums) - 1:
+#                 right_sum = prefix_end[i + 1]
+            
+#             if left_sum == right_sum:
+#                 return i
+        
+#         return -1
