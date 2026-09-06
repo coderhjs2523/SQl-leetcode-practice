08eab1e48f706ee63e911c9937335e4313cb305e
Hareesh <hareeshjaiveersingh@gmail.com>
2026-09-06 15:00:54 +0530

    Time: 3 ms (38.23%) | Memory: 12.3 MB (98.02%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -3,13 +3,15 @@ class Solution(object):
         
         p1 = 0
         p2 = 0
-        if s == "":
-             return True
-        while p1<len(s) and p2 < len(t):
+        count = len(s)
+
+        while p1 < len(s) and p2 < len(t):
+
             if s[p1] == t[p2]:
-                p1 = p1 + 1
-                if p1 == len(s):
-                    return True
-            p2 = p2 + 1
-        return False 
-    
+                count -= 1
+                p1 += 1
+            p2 += 1
+        
+        return count == 0
+
+        
