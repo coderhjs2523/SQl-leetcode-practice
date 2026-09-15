cd7da2d87e6acd518e4590abfb611283e7ad34f1
Hareesh <hareeshjaiveersingh@gmail.com>
2026-09-15 13:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (28.35%) - LeetSync

--- 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.java
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.java
@@ -1,17 +1,19 @@
 class Solution {
     public int minStartValue(int[] nums) {
 
-        int startValue = 0;
-        int ans = 1;
+        int[] prefix_sum = new int[nums.length];
+        prefix_sum[0] = nums[0];
 
-        for (int ele : nums) {
-            startValue += ele;
+        int ans = nums[0];
 
-            if (startValue < 1) {
-                ans = Math.max(ans, 1 - startValue);
-            }
+        for (int i = 1; i < nums.length; i++) {
+            prefix_sum[i] = prefix_sum[i - 1] + nums[i];
+            ans = Math.min(ans, prefix_sum[i]);
         }
 
-        return ans;
+        if (ans <= 0)
+            return Math.abs(ans) + 1;
+
+        return 1;
     }
 }
