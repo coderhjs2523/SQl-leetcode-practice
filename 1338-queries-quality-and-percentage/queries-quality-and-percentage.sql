af2156d4ac766680a21a325b71e336b2c95ff280
Hareesh <hareeshjaiveersingh@gmail.com>
2026-09-19 15:11:36 +0530

    Time: 362 ms (86.91%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,16 +1,11 @@
-SELECT 
-    query_name,
-    ROUND(
-        SUM(rating / position) / COUNT(query_name),
-        2
-    ) AS quality,
-    ROUND(
-        100 * SUM(
+SELECT query_name,
+    ROUND(SUM(rating / position) / COUNT(query_name),2) AS quality,
+    ROUND(SUM(
             CASE
                 WHEN rating < 3 THEN 1
                 ELSE 0
             END
-        ) / COUNT(query_name),
+        ) / COUNT(query_name) * 100,
         2
     ) AS poor_query_percentage
 FROM Queries
