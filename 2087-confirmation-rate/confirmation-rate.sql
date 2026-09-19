9c7e7020b0a48936193eab53ff35e45df0ffbc0b
Hareesh <hareeshjaiveersingh@gmail.com>
2026-09-19 12:59:37 +0530

    Time: 662 ms (87.69%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,4 +1,9 @@
-SELECT S.user_id, ROUND(AVG(if(C.action = 'confirmed', 1, 0)),2) AS confirmation_rate
+-- SELECT S.user_id, ROUND(AVG(if(C.action = 'confirmed', 1, 0)),2) AS confirmation_rate
+-- FROM Signups S
+-- LEFT JOIN Confirmations C ON S.user_id = C.user_id
+-- GROUP BY S.user_id;
+
+SELECT S.user_id, ROUND(AVG(IF(C.action = 'confirmed', 1, 0)),2) AS confirmation_rate
 FROM Signups S
-LEFT JOIN Confirmations C ON S.user_id = C.user_id
-GROUP BY S.user_id;
+LEFT JOIN Confirmations C ON S.user_id  = C.user_id
+GROUP BY S.user_id;  
