6cf2bd827d25b64249dda3477ef530b5e3c03300
Hareesh <hareeshjaiveersingh@gmail.com>
2026-09-19 13:03:19 +0530

    Time: 1159 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,4 +1,4 @@
-SELECT C.id, C.movie, C.description, C.rating 
-FROM Cinema C
-WHERE id % 2 = 1 AND C.description != 'boring'
-ORDER BY C.rating desc;
+SELECT *
+FROM Cinema
+WHERE id % 2 != 0 AND description != 'boring'
+ORDER BY rating DESC;
