6dd2caa7ed45b142d75ddf6cf7075678aafbc492
Hareesh <hareeshjaiveersingh@gmail.com>
2026-09-24 13:24:08 +0530

    Time: 454 ms (95.98%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,16 @@
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16'
+
+UNION
+
+SELECT product_id, new_price AS price
+FROM
+    (
+    SELECT product_id, new_price,
+    ROW_NUMBER() OVER ( PARTITION BY product_id ORDER BY change_date DESC ) AS rn
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    ) T
+WHERE rn = 1;
