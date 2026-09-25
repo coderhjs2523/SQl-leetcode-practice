8ee61c851f72688ad88098284ab0895646b23052
Hareesh <hareeshjaiveersingh@gmail.com>
2026-09-25 09:57:03 +0530

    Time: 3 ms (62.40%) | Memory: 14 MB (54.10%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,13 +1,18 @@
 class Solution(object):
     def singleNumber(self, nums):
-        map = {}
+        # map = {}
 
-        for ele in nums:
-            if ele in map:
-                map[ele] += 1
-            else:
-                map[ele] = 1
+        # for ele in nums:
+        #     if ele in map:
+        #         map[ele] += 1
+        #     else:
+        #         map[ele] = 1
         
-        for key in map.keys():
-            if map[key] == 1:
-                return key
+        # for key in map.keys():
+        #     if map[key] == 1:
+        #         return key
+
+        ans = nums[0]
+        for i in range(1, len(nums)):
+            ans ^= nums[i]
+        return ans
